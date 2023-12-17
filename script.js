0ca10111698161a7968f9dd8f96827a5c96d554e
amanborkar99 <61972395+amanborkar99@users.noreply.github.com>
2023-12-18 02:01:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function submitForm() {
     };
 
     // Make API call to register user and make payment
-    fetch('http://52.66.152.113:3000/register', {
+    fetch('/api/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
