82ecda57a41c7de28a57cd1e965d2d6e5df18813
Aman Borkar <amanborkar99@gmail.com>
2023-12-17 22:33:08 +0530

    fourth commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function submitForm() {
+    // Get form data
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const batch = document.getElementById('batch').value;
+
+    // Prepare data to be sent to the server
+    const formData = {
+        name,
+        age,
+        batch,
+    };
+
+    // Make API call to register user and make payment
+    fetch('https://your-heroku-app-name.herokuapp.com/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        // Handle the response from the server
+        if (data.success) {
+            alert('Registration and payment successful!');
+        } else {
+            alert('Registration failed. Please try again.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle errors
+        alert('An error occurred. Please try again later.');
+    });
+}
